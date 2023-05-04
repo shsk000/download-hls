1bf5f12f1c6cabfa0f32deb02615e10f0dd030c0
shsk000 <02luvsic@gmail.com>
2023-05-04 16:29:06 +0900

    add: settings for developing chrome extentions

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { crx, defineManifest } from "@crxjs/vite-plugin";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const manifest = defineManifest({
+  manifest_version: 3,
+  name: "download-hls",
+  version: "0.0.1",
+  permissions: [],
+  action: {
+    default_popup: "index.html",
+  },
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react(), crx({ manifest })],
+});
